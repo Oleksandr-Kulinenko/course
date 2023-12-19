c727d5f9a20a5307a3fc9792199b9737c18e483f
Oleksandr Kulinenko <sanyakulinenko@ukr.net>
2023-12-19 16:24:53 +0200

    change logic home task 3

--- home_task3/index.php
+++ home_task3/index.php
@@ -1,39 +1,47 @@
 <?php
 
-function create_arr_fibonacci($num,$max_num=34){// second args for max num count arr fibonacci
-    if($num<=$max_num){
-        if($num>=2){
-            $arr = array(0,1);
-            for($i=0;$i<$num;$i++){
-                if($i==0){
-                    $res_arr = array(0,1);
-                }
-                if(count($res_arr)<$num){
-                    $next_elem = array_sum($arr);
-                    array_push($res_arr,$next_elem);
-                    $last_elem = array_pop($arr);
-                    $arr = array($last_elem,$next_elem);
-                }
-            }
-
-            // for check str fibonacci
-            $str_fib_out = '';
-            for($k=0;$k<count($res_arr);$k++){
-                $str_fib_out .= $res_arr[$k].",";
-            }
-            $str_fib_final = substr($str_fib_out,0,-1);// in variable str fibonacci for check first part home work
-
-            return $res_arr;
-        }else{
-            return array(0);
+function create_arr_fibonacci($num){// second args for max num count arr fibonacci
+    for($i=1;$i<$num;$i++) {
+        if ($i == 1) {
+            $arr = array(0);
         }
-    }else{
-        return array(0);
+
+        $sum_arr = array_sum($arr);
+
+        if ($sum_arr < 1 || $sum_arr == 1) {
+            $next_elem = 1;
+            array_push($arr, $next_elem);
+        } else {
+            $last_elem = end($arr);
+            $prev_elem = prev($arr);
+
+            $next_elem = $last_elem + $prev_elem;
+            array_push($arr, $next_elem);
+        }
+    }
+
+    // for check str fibonacci
+    $str_fib_out = '';
+    for($k=0;$k<count($arr);$k++){
+        $str_fib_out .= $arr[$k].",";
+    }
+    $str_fib_final = substr($str_fib_out,0,-1);// in variable str fibonacci for check first part home task
+
+    return $arr;
+}
+
+function sum_fibonacci($num,$max_num=34)
+{
+    if ($num <= $max_num) {
+        if ($num >= 2) {
+            echo "Сума ряду фібоначчі: " . array_sum(create_arr_fibonacci($num));//function create arr fibonacci can accept two args, second args for max num count arr fibonacci, default 34
+        }elseif($num<1){
+            echo "Для підрахунку суми ряду потрібно ввести число більше від 0";//error if count for sum
+        }elseif($num==1){
+            echo "Сума одного елемента дорівнює 0";//for
+        }
+    } else {
+        echo "Перевищено максимальну довжину ряду фібоначчі, максимальна довжина складає 34 елементи";//error for input more max count arr fibonacci
     }
 }
-
-function sum_fibonacci($num){
-    echo "Сума ряду фібоначчі: ".array_sum(create_arr_fibonacci($num));//function create arr fibonacci can accept two args, second args for max num count arr fibonacci, default 34
-}
-
-sum_fibonacci(15);
+sum_fibonacci(35);
